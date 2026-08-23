cd4a38e668dc24a53714a19175a9de3caab04acc
mehedicode <mahdihasanmehedi07@gmail.com>
2026-08-23 14:36:39 +0600

    if else in one line

---
+++ tarnary.js
@@ -0,0 +1,10 @@
+const age = 18;
+
+// if ( age >=18){
+//     console.log('you can vote')
+// }
+// else{
+//     console.log("ghumai thako")
+// }
+
+age >=18 ? console.log('You Can Vote') : console.log('ghumai thako')
