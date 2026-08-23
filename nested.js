1ca070df04e1dc286f28e7c4b06c71f9893934e2
mehedicode <mahdihasanmehedi07@gmail.com>
2026-08-23 13:24:33 +0600

    learn multiple if else

---
+++ nested.js
@@ -0,0 +1,19 @@
+const money = 0;
+
+if (money > 300) {
+    console.log('I love you too')
+}
+
+else {
+    if (money > 100){
+        console.log('dekhi pore janabo')
+    }
+    else {
+        if ( money > 0){
+            console.log("chupppp")
+        }
+        else {
+            console.log( "thappor khabi????")
+        }
+    }
+}
