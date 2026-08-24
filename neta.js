0d2b3bb0fdeb7f83dd3edf4697e6e3bc62a486ef
mehedicode <mahdihasanmehedi07@gmail.com>
2026-08-24 13:03:24 +0600

    simple

--- neta.js
+++ neta.js
@@ -1,12 +1,17 @@
 let price = 500;
 const isLeader = false;
 
-if ( isLeader === true){
-    price = 0;
-}
+// if ( isLeader === true){
+//     price = 0;
+// }
 
-else {
-    price = price + 100;
-}
+// else {
+//     price = price + 100;
+// }
+
+// console.log(price)
+
+
+price = isLeader === true ? 0 : price + 100;
 
 console.log(price)
