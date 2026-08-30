206d21a6bcd6695fbdac1520c0f0632709746667
mehedicode <mahdihasanmehedi07@gmail.com>
2026-08-30 15:45:34 +0600

    complete Task 1

---
+++ task.js
@@ -0,0 +1,11 @@
+// task 1 //
+var burger = 550;
+
+// if(burger > 500){
+//     console.log("coke is free")
+// }
+// else {
+//     console.log("coke is 30")
+// }
+
+console.log(burger > 500 ? "coke is free" : 'coke is 30')
