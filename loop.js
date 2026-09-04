e91fa3272f2dfddc7ee45dee801b5437b6491b78
mehedicode <mahdihasanmehedi07@gmail.com>
2026-09-04 17:05:34 +0600

    loop practice

--- loop.js
+++ loop.js
@@ -1,13 +1,22 @@
-const numbers = [2,4,6,8,9,0];
+// const numbers = [2,4,6,8,9,0];
 
-console.log(numbers);
+// console.log(numbers);
 
-for (const number of numbers){
-    console.log(number);
-}
+// for (const number of numbers){
+//     console.log(number);
+// }
 
-let num = 0;
-while (num < 5){
-    console.log('loooop', num);
-    num = num + 1;
+// let num = 0;
+// while (num < 5){
+//     console.log('loooop', num);
+//     num = num + 1;
+// }
+
+let num = 1;
+let sum = 0
+while (num < 10){
+    console.log(num);
+    sum = sum + num;
+    console.log('sum =' ,sum);
+    num++;
 }
